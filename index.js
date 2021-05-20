8bc8ec42b7cb87ad95ea725bc992d23c40973336
sifatnabil <sifatnabil@gmail.com>
2021-05-20 18:31:25 +0600

    Added more filtering for links

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const fs = require("fs");
 const firstname = "karen",
   lastname = "voigt";
 
-const sites = ["PubMed", "PubFacts", "TrialsJournal"];
+const sites = [
+  { site: "PubMed", mainUrl: "https://pubmed.ncbi.nlm.nih.gov/" },
+  { site: "PubFacts", mainUrl: "https://www.pubfacts.com/" },
+  {
+    site: "TrialsJournal",
+    mainUrl: "https://trialsjournal.biomedcentral.com/",
+  },
+];
 
 const searchName = async () => {
   const browser = await puppeteer.launch({
@@ -15,41 +22,65 @@ const searchName = async () => {
   });
 
   const page = await browser.newPage();
+  const allLinks = [];
 
   for (const site of sites) {
-    const sitename = site;
+    const sitename = site.site;
+    const sitehome = site.mainUrl;
 
     const queryString = QueryString(firstname, lastname, sitename);
 
     await page.goto(queryString);
-    await page.waitForTimeout(8000);
+    await page.waitForTimeout(3000);
 
-    const aTags = await page.evaluate(() => {
-      const tags = document.querySelectorAll("a");
-      const links = [];
-      tags.forEach((tag) => {
-        const text = tag.outerText.trim();
-        const link = tag.href;
-        if (text.split(" ").length >= 5) {
-          links.push({
-            Link: link,
-            Text: text,
-          });
-        }
-      });
-      return links;
-    });
+    const aTags = await page.evaluate(
+      (name, home) => {
+        const tags = document.querySelectorAll("body a");
+        const links = [];
+        const linksWithText = [];
+        tags.forEach((tag) => {
+          const text = tag.outerText.trim();
+          const link = tag.href;
 
-    const filename = `data/${firstname}_${lastname}_${sitename}_length5.txt`;
+          if (
+            text.split(" ").length >= 4 &&
+            link.includes(home) &&
+            !links.includes(link)
+          ) {
+            linksWithText.push({
+              Link: link,
+              Text: text,
+              Site: name,
+            });
+            links.push(link);
+          }
+        });
+        return linksWithText;
+      },
+      sitename,
+      sitehome
+    );
 
-    const file = fs.createWriteStream(filename);
-    aTags.forEach((line) => {
-      file.write(`${line.Link}, ${line.Text};\n`);
-    });
-    file.end();
+    allLinks.push(...aTags);
 
     console.log(aTags);
   }
+
+  const foldername = `data/${firstname}_${lastname}`;
+  if (allLinks.length > 0 && !fs.existsSync(foldername)) {
+    fs.mkdirSync(foldername);
+  }
+
+  const filename = `${foldername}/${firstname}_${lastname}.json`;
+  fs.writeFile(filename, JSON.stringify(allLinks), () => {});
+
+  const linksFile = fs.readFileSync(filename);
+  const parsedFile = JSON.parse(linksFile);
+  console.log(parsedFile);
+
+  // for (const obj of parsedFile) {
+  //   const link = obj.link;
+  // }
 
   await browser.close();
 };
